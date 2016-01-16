82c62dffb5f6656eb8091d3b6d620f3bb26739dc
Arika <Delete0093@Gmail.com>
2016-01-16 22:50:17 +0900

    loaderの追加など + loading...の追加 * cssの変更(主にpadding y)

---
+++ js/loader.js
@@ -0,0 +1,16 @@
+// loading...を表示する
+$(function () {
+	// 全ての読み込み完了後に呼ばれる関数
+	var loadfin = function () { 
+		var pageH = $("body").height() + 30;
+
+		$("#fade").css("height", pageH).delay(400).fadeOut(300);
+		$("#loader").delay(300).fadeOut(150);
+		$("#loadnoscr").delay(300).fadeOut(150);
+	};
+
+	jQuery.event.add(window, "load", loadfin);
+
+	// 3秒後に強制で終了
+	setTimeout(loadfin, 2800);
+});
