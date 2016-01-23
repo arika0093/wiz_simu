ee22e92030baa3b8dda8d96698edad1a9b5f5813
Arika <Delete0093@Gmail.com>
2016-01-24 00:05:13 +0900

    修正忘れ直し

--- js/pagetemp.js
+++ js/pagetemp.js
@@ -1,11 +1,9 @@
 // template page loader
 $(function () {
-	var url_b = (window.location.href.indexOf("/pages/") >= 0) ? ".." : "";
-
-	$("#Header").load(url_b + "/template/header.html");
-	$("#Footer").load(url_b + "/template/footer.html");
+	$("#Header").load("./template/header.html");
+	$("#Footer").load("./template/footer.html");
 	if ($("#Updatelog")) {
-		$.get(url_b + "update.txt", function (data) {
+		$.get("update.txt", function (data) {
 			var log = data.replace(/\n/g, "<br/>\n");
 			$("#Updatelog").html(log);
 		}, "text");
