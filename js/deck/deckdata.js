271719a3935e8ec05f415b13c2c4d6657e955eb3
Arika <Delete0093@Gmail.com>
2016-03-08 01:25:33 +0900

    URL短縮の枠組み作成

--- js/deck/deckdata.js
+++ js/deck/deckdata.js
@@ -1,21 +1,96 @@
-function deckdata_template() {
-	// URLに保存する最低限のカード情報
-	return $.extend(true, {}, {
-		id: -1,
-		mana: 200,
-		awake: 0,
-		crystal: [],
+// ------------------------------------
+// テンプレート生成
+// ------------------------------------
+// dataのテンプレートを生成する
+function deckdata_DataTemplate(size) {
+	size = size || 5;
+	var data = {
+		quest: "",
+		quest_imple: {},
+		deck: [],
+	};
+	for (var i = 0; i < size; i++) {
+		data.deck.push($.extend(true, {}, {
+			cardno: -1,
+			card_imple: {},
+			mana: 200,
+			awake: 0,
+			awake_default: 0,
+			crystal: [],
+		}));
+	}
+	return data;
+}
+
+// ------------------------------------
+// 各種操作関数
+// ------------------------------------
+// data保存
+function deckdata_Save(data) {
+	// AJAXを使ってPHPを呼ぶ
+	$.ajax({
+		type: "POST",
+		url: "/api/shorten.php",
+		data: "t=set&d=" + JSON.stringify(data),
 	});
 }
 
-function deckdata_create(deck, mana_array, awake_array) {
-	var cards = [];
-	for (var i = 0; i < deck.length; i++) {
-		var c_tmp = deckdata_template();
-		c_tmp.id = deck.id;
-		c_tmp.mana = mana_array[i];
-		c_tmp.awake = awake_array[i];
-		cards.push(c_tmp);
+// data読み込み
+function deckdata_Load(short) {
+	// AJAXを使ってPHPを呼ぶ
+	$.ajax({
+		type: "POST",
+		url: "/api/shorten.php",
+		data: "t=get&d=" + short,
+	});
+}
+
+// ------------------------------------
+// 互換維持用
+// ------------------------------------
+// 旧版URL読み込み
+function deckdata_LoadOldUrl(oldquery) {
+	var data = deckdata_DataTemplate(5);
+	var q = deckdata_GetOldQuery(oldquery);
+	if (q.length > 0) {
+		for (var ct = 0; ct < 5; ct++) {
+			var dd = data.deck[ct];
+			// | で区切る
+			var q_spl = q[ct].split("|");
+			// 精霊番号
+			var cno = Number(q_spl[0]);
+			if (cno == 0) { continue; }
+			var card = $.extend(true, {}, $.grep(Cards, function (e, i) {
+				return e.cardno == cno;
+			})[0]);
+			if (card) {
+				var awake_num = card.awakes.length;
+				dd.id = card.cardno;
+				dd.mana = (q_spl[1] ? Number(q_spl[1]) : 200);
+				dd.awake = awake_num;
+				dd.awake_default = (q_spl[2] ? Number(q_spl[2]) : awake_num);
+			}
+		}
 	}
-	return cards;
+	if (q.length >= 6) {
+		data.quest = $.grep(Quests, function (e, i) {
+			return e.id == q[5];
+		})[0];
+	}
+	return data;
+}
+
+// 旧版URL query読み込み
+function deckdata_GetOldQuery(qr) {
+	var result = [];
+	var q = qr ? qr : window.location.search;
+	if (1 < q.length) {
+		var query = q.substring(1);
+		var parameters = query.split(',');
+		for (var i = 0; i < parameters.length; i++) {
+			var param = decodeURIComponent(parameters[i]);
+			result[i] = param;
+		}
+	}
+	return result;
 }
