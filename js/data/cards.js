4c0b13742026679860c536b23b6fcf2cd338209c
rinshan <rinshandream@gmail.com>
2016-05-28 05:53:10 +0900

    レイドラヒルメ
    精霊強化の軽減効果は未実装？だけど定義してあります。

--- js/data/cards.js
+++ js/data/cards.js
@@ -31873,4 +31873,47 @@ Cards = [{
 		Statusup(500,0),
 		Attr_statusup(0,100, [0,0,1,0,0,]),
 	],
+},{
+	name: "AbCd-Θ:《夜藝尽焔天 ラヒルメ》",
+	cardno: 6297,
+	imageno: 8144,
+	hp: 3614,
+	atk: 3030,
+	cost: 65,
+	attr: [0,4],
+	species: [11],
+	islegend: true,
+	as1: {
+		desc: "4チェインで火属性の攻撃力をアップ、複属性が闇属性だとさらにアップ(10%/120%)",
+		proc: ChainEnhance_SubAttr(0.1, 1.2, [1,0,0,0,0], [0,0,0,0,1], 4),
+	},
+	ss1: {
+		desc: "<精霊強化>10ターンの間、味方全体のダメージを軽減(25%)◆発動中行動不可",
+		turn: 6,
+		proc: [ss_reinforcement_all(0, 0.25, [1,1,1,1,1], 10)],
+	},
+	awakes: [
+		Panel_boost([1,0,0,0,0,],1),
+		Statusup(0,200),
+		Panel_boost([1,0,0,0,0,],2),
+		Statusup(200,0),
+		Attr_statusup(100,0, [1,0,0,0,0,]),
+		NEFTJOD(30),
+		Attr_statusup(200,0, [1,0,0,0,0,]),
+		Attr_relief([0,1,0,0,0,],10),
+		Spec_statusup(0,200, [11,]),
+	],
+	as2: {
+		desc: "4チェインで火属性の攻撃力をアップ、複属性が闇属性だとさらにアップ(40%/150%)",
+		proc: ChainEnhance_SubAttr(0.4, 1.5, [1,0,0,0,0], [0,0,0,0,1], 4),
+	},
+	ss2: {
+		desc: "<精霊強化>10ターンの間、味方全体のダメージを軽減(40%)◆発動中行動不可",
+		turn: 9,
+		proc: [ss_reinforcement_all(0, 0.40, [1,1,1,1,1], 10)],
+	},
+	Lawake: [
+		Attr_relief([1,1,1,1,1,],10),
+		Statusup(0,500),
+	],
 },];
