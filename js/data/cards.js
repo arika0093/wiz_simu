b2991df1343f165ba93afed8390e1c55b6ab6183
konta0112 <takemuraoooo@gmail.com>
2016-05-28 19:46:59 +0900

    ラヒルメ, ウリシラの行動定義

--- js/data/cards.js
+++ js/data/cards.js
@@ -30544,7 +30544,6 @@ Cards = [{
 		Attr_statusup(0,100, [1,0,0,0,0,]),
 	],
 },
-/*
 {
 	name: "終末の白き音色 ウリシラ・ファーレ",
 	cardno: 6194,
@@ -30562,7 +30561,7 @@ Cards = [{
 	ss1: {
 		desc: "<精霊強化>4ターンの間、味方全体を徐々に回復し、ダメージを軽減(20%、10%)◆発動中行動不可",
 		turn: 6,
-		proc: [null],
+		proc: [ss_reinforcement_all(4, [ss_regenerate(0.2, 4, "RF"), ss_attr_guard([1,1,1,1,1], 0.1, 4, "RF")])],
 	},
 	awakes: [
 		Statusup(200,0),
@@ -30583,13 +30582,15 @@ Cards = [{
 	ss2: {
 		desc: "<精霊強化>4ターンの間、味方全体を徐々に回復し、ダメージを軽減(30%、10%)。5チェインを消費しさらに攻撃力アップ(50%)◆発動中行動不可",
 		turn: 9,
-		proc: [null],
+		proc: [ss_chain_cost_skill(5,
+			ss_reinforcement_all(4, [ss_regenerate(0.3, 4, "RF"), ss_attr_guard([1,1,1,1,1], 0.1, 4, "RF"), ss_enhance_all(0.5, 4, [1,1,1,1,1], "RF")]),
+			ss_reinforcement_all(4, [ss_regenerate(0.3, 4, "RF"), ss_attr_guard([1,1,1,1,1], 0.1, 4, "RF")])
+		)],
 	},
 	Lawake: [
 		Statusup(1000,0),
 	],
 },
-*/
 {
 	name: "処刑台の麗花 イーディス・キルティ",
 	cardno: 6200,
@@ -31892,7 +31893,7 @@ Cards = [{
 	ss1: {
 		desc: "<精霊強化>10ターンの間、味方全体のダメージを軽減(25%)◆発動中行動不可",
 		turn: 6,
-		proc: [ss_reinforcement_all(0, 0.25, [1,1,1,1,1], 10)],
+		proc: [ss_reinforcement_all(10, [ss_attr_guard([1,1,1,1,1], 0.25, 10, "RF")])],
 	},
 	awakes: [
 		Panel_boost([1,0,0,0,0,],1),
@@ -31912,7 +31913,7 @@ Cards = [{
 	ss2: {
 		desc: "<精霊強化>10ターンの間、味方全体のダメージを軽減(40%)◆発動中行動不可",
 		turn: 9,
-		proc: [ss_reinforcement_all(0, 0.40, [1,1,1,1,1], 10)],
+		proc: [ss_reinforcement_all(10, [ss_attr_guard([1,1,1,1,1], 0.4, 10, "RF")])],
 	},
 	Lawake: [
 		Attr_relief([1,1,1,1,1,],10),
