57fe6004a568821b7cbcc537b42757805f127817
konta0112 <takemuraoooo@gmail.com>
2016-06-05 21:03:02 +0900

    スザクノーマル2つ追加、表示順変更

--- js/data/quests/#order.js
+++ js/data/quests/#order.js
@@ -2,35 +2,34 @@ Quests.forEach(function(e){
     // 配置の定義
     var QuestOrder = {
         category:["tornament", "past event", "other"],
-        name:["真覇", "覇級", "絶級"]
     }
     
     // カテゴリ順
     e.sortKey = []
     e.sortKey[0] = (QuestOrder.category.reverse().indexOf(e.category) + 1)
     
-    // tornamentは後ろ4文字で纏める
-    // tornament以外はざっくりと頭の8文字で纏める
+    // tornamentは後ろ4文字で並べる
+    // past eventは前8文字で並べる
     if(e.category == "tornament"){
         e.sortKey[1] = e.name.slice(-4)
+    }else if(e.category == "past event"){
+        e.sortKey[1] = e.name.slice(0,8)
     }else{
-        e.sortKey[1] = e.name.slice(0, 8)
+        e.sortKey[1] = 0
     }
     
-    // イベントトナメ等の覇とか絶とかの考慮
-    var tmpNum = 0
-    QuestOrder.name.reverse().forEach(function(f, n){
-        tmpNum = e.name.indexOf(f) == -1 ? tmpNum : n + 1
-    })
-    e.sortKey[2] = tmpNum
+    // あとはid順（昇順）
+    e.sortKey[2] = e.id
 })
 
 Quests = Quests.sort(function(a, b){
     var x = a.sortKey;
     var y = b.sortKey;
+    var sign;
     for(n = 0; n < x.length; n++){
-        if(x[n] < y[n]) return 1;
-        if(x[n] > y[n]) return -1;
+        sign = n == 2 ? -1 : 1;
+        if(x[n] < y[n]) return sign;
+        if(x[n] > y[n]) return -sign;
     }
     return 0
 })
