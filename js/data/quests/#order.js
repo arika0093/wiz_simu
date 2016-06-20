513b572b0c6e0178f69897455d937d054e1e15ff
konta0112 <takemuraoooo@gmail.com>
2016-06-20 23:06:16 +0900

    fix order

--- js/data/quests/#order.js
+++ js/data/quests/#order.js
@@ -9,7 +9,7 @@ $.each(Quests, function(n, e){
     // category内の定義順
     e.sortKey["0"] = (ctjp.sim_index !== undefined ? ctjp.sim_index : 9999);
 	
-     if(e.category === "tornament"){
+    if(e.category === "tornament"){
         e.sortKey["1"] = e.name.slice(-4)
     }else if(e.category === "past_event"){
         e.sortKey["1"] = e.name.slice(0,8)
@@ -17,8 +17,19 @@ $.each(Quests, function(n, e){
         e.sortKey["1"] = 0
     }
    
+    // イベトナメ用
+    e.sortKey["2"] = 999
+    var evOrder=["覇級","絶級","邪眼級","封魔級"]
+    if(e.category === "past_event" || e.category === "evtornament"){
+        for(var n=evOrder.length; n>0; n--){
+            if(e.name.indexOf(evOrder[n])!=-1){
+                e.sortKey["2"] = n;
+            }
+        }
+    }
+
     // あとはid順（昇順）
-    e.sortKey["2"] = e.id
+    e.sortKey["3"] = e.id
 })
 
 Quests = Quests.sort(function(a, b){
@@ -26,7 +37,7 @@ Quests = Quests.sort(function(a, b){
     var y = b.sortKey;
     for(n = 0; n < Object.keys(x).length; n++){
 		var ns = n;
-		var sign = (n != 1 ? -1 : 1);
+		var sign = (n != 1 && n != 2  ? -1 : 1);
         if(x[ns] < y[ns]) return sign;
         if(x[ns] > y[ns]) return -sign;
     }
