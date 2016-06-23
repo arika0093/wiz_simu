37177131725ff4e8ed68fb2312137233c46a32c8
konta0112 <takemuraoooo@gmail.com>
2016-06-23 19:52:45 +0900

    add grimoire

---
+++ js/data/quests/default/material/material_sinpa_grimoire_thunder.js
@@ -0,0 +1,310 @@
+{
+   id: "material_sinpa_grimoire_thunder",
+   name: "魔導書真覇（雷）",
+   desc: "",
+   overlap: false,
+   aprnum: 6,
+   data: [
+      {
+         appearance: [
+            1
+         ],
+         enemy: [
+            {
+               name: "招雷の原典 トパーズ・グリム",
+               hp: 50000,
+               imageno: 2784,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            },
+            {
+               name: "招雷の原典 トパーズ・グリム",
+               hp: 50000,
+               imageno: 2784,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            2
+         ],
+         enemy: [
+            {
+               name: "招雷の原典 トパーズ・グリム",
+               hp: 50000,
+               imageno: 2784,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 60000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "招雷の原典 トパーズ・グリム",
+               hp: 50000,
+               imageno: 2784,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            3
+         ],
+         enemy: [
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 60000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(900, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 60000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(900, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            4
+         ],
+         enemy: [
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 50000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 70000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(900, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 50000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            5
+         ],
+         enemy: [
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 60000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 70000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 60000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            6
+         ],
+         enemy: [
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 70000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 100000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_ss_sealed, "5体のSPスキルを封印する（3T）", 100, false, 5, 4))
+                  ],
+                  on_move: [
+                     s_enemy_attack(500, 5, 3, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "轟雷の輝典 ハイトパーズ・グリム",
+               hp: 70000,
+               imageno: 7611,
+               attr: 2,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      }
+   ]
+}
---
+++ js/data/quests/default/material/material_sinpa_grimoire_water.js
@@ -0,0 +1,310 @@
+{
+   id: "material_sinpa_grimoire_water",
+   name: "魔導書真覇（水）",
+   desc: "",
+   overlap: false,
+   aprnum: 6,
+   data: [
+      {
+         appearance: [
+            1
+         ],
+         enemy: [
+            {
+               name: "蒼凍の原典 サファイア・グリム",
+               hp: 50000,
+               imageno: 2783,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            },
+            {
+               name: "蒼凍の原典 サファイア・グリム",
+               hp: 50000,
+               imageno: 2783,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            2
+         ],
+         enemy: [
+            {
+               name: "蒼凍の原典 サファイア・グリム",
+               hp: 50000,
+               imageno: 2783,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 60000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "蒼凍の原典 サファイア・グリム",
+               hp: 50000,
+               imageno: 2783,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            3
+         ],
+         enemy: [
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 60000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(900, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 60000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(900, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            4
+         ],
+         enemy: [
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 50000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 70000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(900, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 50000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            5
+         ],
+         enemy: [
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 60000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 70000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 60000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(800, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            6
+         ],
+         enemy: [
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 70000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 100000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_ss_sealed, "5体のSPスキルを封印する（3T）", 100, false, 5, 4))
+                  ],
+                  on_move: [
+                     s_enemy_attack(500, 5, 3, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "零結の氷典 ハイサファイア・グリム",
+               hp: 70000,
+               imageno: 7610,
+               attr: 1,
+               spec: 6,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      }
+   ]
+}
