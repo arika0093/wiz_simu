7c006ebefdbeca832f381159c91e49109d17c55b
konta0112 <takemuraoooo@gmail.com>
2016-09-02 07:16:28 +0900

    add chromag6_g1

---
+++ js/data/quests/event/chromag6/chromag6_g1.js
@@ -0,0 +1,277 @@
+{
+   id: "chromag6_g1",
+   name: "お兄さん級 悪ノリに決着を",
+   desc: "",
+   overlap: false,
+   aprnum: 4,
+   data: [
+      {
+         appearance: [
+            1
+         ],
+         enemy: [
+            {
+               name: "魔電鳥",
+               hp: 70000,
+               imageno: 8920,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)/*nogambits*/
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "魔電鳥",
+               hp: 60000,
+               imageno: 8920,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     m_enemy_once(s_enemy_attr_weaken([0,0,1,0,0], 1.25, 5, 4)/* Warning: 属性要確認： 雷属性弱体化25%（3T） */),
+                     s_enemy_attack(400, 3, 3, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            },
+            {
+               name: "魔電鳥",
+               hp: 60000,
+               imageno: 8920,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)/*nogambits*/
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 2
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            2
+         ],
+         enemy: [
+            {
+               name: "邪氷鳥",
+               hp: 50000,
+               imageno: 8919,
+               attr: 1,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     m_enemy_once(s_enemy_attack_attrsp(975, 325, [1,0,0,0,0], 5, 1, false)),
+                     s_enemy_attack(325, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 2
+               }
+            },
+            {
+               name: "邪雷鳥",
+               hp: 70000,
+               imageno: 8921,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(damage_block_own(20000, 4))
+                  ],
+                  on_move: [
+                     s_enemy_attack(1300, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "邪雷鳥",
+               hp: 80000,
+               imageno: 8921,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(3000, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            3
+         ],
+         enemy: [
+            {
+               name: "邪雷鳥",
+               hp: 80000,
+               imageno: 8921,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_chain_break(),
+                     s_enemy_attack(1200, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "邪雷鳥",
+               hp: 100000,
+               imageno: 8921,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_chain_sealed, "-", 100, false, 6))
+                  ],
+                  on_move: [
+                     s_enemy_attack(300, 5, 5, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "邪炎鳥",
+               hp: 70000,
+               imageno: 8917,
+               attr: 0,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_as_sealed(5, 4),
+                     s_enemy_attack(500, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 2
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            4
+         ],
+         enemy: [
+            {
+               name: "邪炎鳥",
+               hp: 250000,
+               imageno: 8917,
+               attr: 0,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(s_enemy_cursed(1000, 5, 3)),
+                     damage_switch(s_enemy_when_dead_s(), m_enemy_angry(), true)
+                  ],
+                  on_move: [
+                     s_enemy_attack(600, 5, 1, true)
+                  ],
+                  on_angry: [
+                     s_enemy_attr_absorb([1,0,0,0,0], 2, 3)
+                  ],
+                  on_move_angry: [
+                     s_enemy_attr_absorb([1,0,0,0,0], 2, 3)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 2
+               }
+            },
+            {
+               name: "クロム・マグナお兄さん＆お姉さん",
+               hp: 800000,
+               imageno: 8909,
+               attr: 2,
+               spec: 9,
+               isStrong: true,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_attack, "-", 100, false, 1200, 5, 10, true)),
+                     damage_switch(s_enemy_when_dead_s(), m_enemy_angry(), true)
+                  ],
+                  on_move: [
+                     s_enemy_attack(1500, 5, 1, true)
+                  ],
+                  on_angry: [
+                     s_enemy_continue_damage(3, 1500, 1500)
+                  ],
+                  on_move_angry: [
+                     s_enemy_poison(1000, 5, 3),
+                     s_enemy_attack(2000, 5, 1, true),
+                     s_enemy_attack(2000, 5, 1, true),
+                     s_enemy_attack(2000, 5, 1, true),
+                     s_enemy_attack(2500, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "魔電鳥",
+               hp: 250000,
+               imageno: 8920,
+               attr: 2,
+               spec: 2,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(damage_block_own(20000, 6)),
+                     damage_switch(s_enemy_when_hpdown(0.5), m_enemy_angry(), true)
+                  ],
+                  on_move: [
+                     s_enemy_attack(300, 5, 4, true)
+                  ],
+                  on_angry: [
+                     attack_counter_dual(700, 6)
+                  ],
+                  on_move_angry: [
+                     s_enemy_attack(300, 5, 4, true),
+                     s_enemy_attack(300, 5, 4, true),
+                     s_enemy_attack(300, 5, 4, true),
+                     s_enemy_attack(300, 5, 4, true),
+                     s_enemy_attack(300, 5, 4, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      }
+   ]
+}
