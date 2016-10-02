ff8ebd6ba6b8d857dea996636edcbbd918b1ee7f
konta0112 <takemuraoooo@gmail.com>
2016-10-02 13:47:43 +0900

    add IR dic copy master -> dev

--- tools/imgReader/dicRenew.sh
+++ tools/imgReader/dicRenew.sh
@@ -12,3 +12,4 @@ echo "
 
 cd `dirname $0`
 setarch `uname -m` -R ./sImgReader.out /r
+cp -r /virtual/mu51027jkc/public_html/wiztools.net/tools/imgReader/dic /virtual/mu51027jkc/public_html/dev.wiztools.net/tools/imgReader/dic
