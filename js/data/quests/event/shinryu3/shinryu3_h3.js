70f1cc3d98c3595d359ccdbd31948df3f691f43a
konta0112 <takemuraoooo@gmail.com>
2016-11-17 01:20:39 +0900

    add shinryu3

---
+++ js/data/quests/event/shinryu3/shinryu3_h3.js
@@ -0,0 +1,269 @@
+{
+   id: "shinryu3_h3",
+   name: "上級 王を賭けた戦い",
+   desc: "",
+   overlap: false,
+   aprnum: 4,
+   data: [
+      {
+         appearance: [
+            1
+         ],
+         enemy: [
+            {
+               name: "雷撃剛竜",
+               hp: 50000,
+               imageno: 9484,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 2
+               }
+            },
+            {
+               name: "雷撃剛竜",
+               hp: 50000,
+               imageno: 9484,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            },
+            {
+               name: "稲妻のヘイトフルドラゴン",
+               hp: 50000,
+               imageno: 9485,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true),
+                     s_enemy_poison(500, 5, 3)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            2
+         ],
+         enemy: [
+            {
+               name: "火炎剛竜",
+               hp: 50000,
+               imageno: 9480,
+               attr: 0,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(s_enemy_force_reservoir())
+                  ],
+                  on_move: [
+                     m_enemy_once(s_enemy_attack(1500, 5, 1, true)),
+                     s_enemy_attack(750, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "痺河竜",
+               hp: 60000,
+               imageno: 9490,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(300, 3, 5, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            },
+            {
+               name: "騒がしいヒッポポドラゴン",
+               hp: 70000,
+               imageno: 9491,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_as_sealed(5, 4),
+                     s_enemy_attack(300, 3, 4, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            3
+         ],
+         enemy: [
+            {
+               name: "騒がしいヒッポポドラゴン",
+               hp: 80000,
+               imageno: 9491,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_continue_damage, "-", 100, false, 5, 2000, 2000))
+                  ],
+                  on_move: [
+                     s_enemy_attack(1500, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "稲妻のヘイトフルドラゴン",
+               hp: 90000,
+               imageno: 9485,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(s_enemy_attr_weaken([0,0,1,0,0], 1.5, 5, 4)/*!*/)
+                  ],
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "燃えるヒッポポドラゴン",
+               hp: 50000,
+               imageno: 9487,
+               attr: 0,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_deathlimit(5, 4),
+                     s_enemy_attack(600, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 2
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            4
+         ],
+         enemy: [
+            {
+               name: "稲妻のヘイトフルドラゴン",
+               hp: 90000,
+               imageno: 9485,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_all_sealed, "-", 100, false, 5, 5))
+                  ],
+                  on_move: [
+                     s_enemy_as_sealed(3, 4),
+                     s_enemy_attack(1000, 5, 1, true),
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "竜骸獣 ログオーズ",
+               hp: 600000,
+               imageno: 9461,
+               attr: 2,
+               spec: 7,
+               isStrong: true,
+               move: {
+                  on_popup: [
+                     m_enemy_once(attack_counter_dual(700, 11)),
+                     damage_switch(s_enemy_when_hpdown(0.5), m_enemy_angry(), true)
+                  ],
+                  on_move: [
+                     s_enemy_force_reservoir(),
+                     s_enemy_attack(3000, 5, 1, true)
+                  ],
+                  on_angry: [
+                     s_enemy_attr_weaken([0,0,1,0,0], 1.5, 5, 4)/*!*/
+                  ],
+                  on_move_angry: [
+                     s_enemy_attack(1500, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "騒がしいヒッポポドラゴン",
+               hp: 20,
+               imageno: 9491,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(impregnable(99)),
+                     damage_switch(s_enemy_when_dead_s(), m_enemy_angry(), true)
+                  ],
+                  on_move: [
+                     s_enemy_poison(2000, 5, 3),
+                     s_enemy_attack(291.6666666666667, 4, 3, true)
+                  ],
+                  on_angry: [
+                     s_enemy_panicshout(0, 5, 3)
+                  ],
+                  on_move_angry: [
+                     s_enemy_attack(328.125, 4, 4, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      }
+   ]
+}
---
+++ js/data/quests/event/shinryu3/shinryu3_h5.js
@@ -0,0 +1,278 @@
+{
+   id: "shinryu3_h5",
+   name: "絶級 覚醒！",
+   desc: "",
+   overlap: false,
+   aprnum: 4,
+   data: [
+      {
+         appearance: [
+            1
+         ],
+         enemy: [
+            {
+               name: "鱗水剛竜",
+               hp: 50000,
+               imageno: 9482,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "鱗水剛竜",
+               hp: 60000,
+               imageno: 9482,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1100, 5, 1, true),
+                     s_enemy_chain_sealed(4)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "鱗酷のヘイトフルドラゴン",
+               hp: 50000,
+               imageno: 9483,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            2
+         ],
+         enemy: [
+            {
+               name: "鱗酷のヘイトフルドラゴン",
+               hp: 60000,
+               imageno: 9483,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_attack(2000, 1, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 2
+               }
+            },
+            {
+               name: "潤河竜",
+               hp: 80000,
+               imageno: 9488,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(damage_block_own(20000, 4))
+                  ],
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "潤河竜",
+               hp: 60000,
+               imageno: 9488,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(damage_block_own(15000, 4))
+                  ],
+                  on_move: [
+                     s_enemy_attack(300, 3, 3, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 2
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            3
+         ],
+         enemy: [
+            {
+               name: "静かなヒッポポドラゴン",
+               hp: 120000,
+               imageno: 9489,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_cursed, "-", 100, false, 3000, 5, 5))
+                  ],
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "静かなヒッポポドラゴン",
+               hp: 100000,
+               imageno: 9489,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     s_enemy_force_reservoir(),
+                     s_enemy_attack(3000, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 2,
+                  wait: 1
+               }
+            },
+            {
+               name: "雷撃剛竜",
+               hp: 70000,
+               imageno: 9484,
+               attr: 2,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(s_enemy_chain_sealed(4))
+                  ],
+                  on_move: [
+                     s_enemy_attack(500, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      },
+      {
+         appearance: [
+            4
+         ],
+         enemy: [
+            {
+               name: "静かなヒッポポドラゴン",
+               hp: 200000,
+               imageno: 9489,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_move: [
+                     m_enemy_once(s_enemy_as_sealed(5, 6)),
+                     s_enemy_attack(300, 5, 3, true)
+                  ],
+                  on_popup: [
+                     damage_switch(s_enemy_when_dead_s(), m_enemy_angry(), true)
+                  ],
+                  on_move_angry: [
+                     s_enemy_attr_weaken([0,1,0,0,0], 1.5, 5, 4)/*!*/,
+                     s_enemy_attack(300, 5, 3, true),
+                     s_enemy_attack(300, 5, 3, true),
+                     s_enemy_attack(300, 5, 3, true),
+                     s_enemy_attack(300, 5, 3, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "歪んだ竜刃 ガンボ・スヴォラク",
+               hp: 700000,
+               imageno: 9469,
+               attr: 1,
+               spec: 0,
+               isStrong: true,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_cursed, "-", 100, false, 5000, 5, 5)),
+                     damage_switch(s_enemy_when_hpdown(0.5), m_enemy_angry(), true)
+                  ],
+                  on_move: [
+                     s_enemy_attack(1200, 5, 1, true)
+                  ],
+                  on_angry: [
+                     s_enemy_discharge(5, 3)
+                  ],
+                  on_move_angry: [
+                     damage_block_own(30000, 4),
+                     s_enemy_attack(1500, 5, 1, true),
+                     s_enemy_attack(1500, 5, 1, true),
+                     s_enemy_panicshout(0, 5, 3),
+                     s_enemy_attack(1500, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            },
+            {
+               name: "静かなヒッポポドラゴン",
+               hp: 300000,
+               imageno: 9489,
+               attr: 1,
+               spec: 0,
+               isStrong: false,
+               move: {
+                  on_popup: [
+                     m_enemy_once(skill_counter_func(s_enemy_all_sealed, "-", 100, false, 5, 6)),
+                     damage_switch(s_enemy_when_dead_s(), m_enemy_angry(), true)
+                  ],
+                  on_move: [
+                     s_enemy_attack(1000, 5, 1, true)
+                  ],
+                  on_angry: [
+                     s_enemy_discharge(5, 3)
+                  ],
+                  on_move_angry: [
+                     s_enemy_attack(1250, 5, 1, true)
+                  ],
+                  atrandom: false,
+                  turn: 1,
+                  wait: 1
+               }
+            }
+         ]
+      }
+   ]
+}
