427d5bb16ae774df943153d9c7cd6084380f20cf
rinshan <rinshandream@gmail.com>
2017-04-19 17:22:04 +0900

    微修正

--- js/data/cards.js
+++ js/data/cards.js
@@ -69670,7 +69670,7 @@ Cards = [{
 	ss1: {
 		desc: "<精霊強化>5ターンの間、味方全体を徐々に回復(20%)、さらに攻撃力とHPを250アップ(上限値:2000)◆発動中行動不可",
 		turn: 5,
-		proc: ss_reinforcement_all(5, [ss_regenerate(0.2, 5, "RF"), ss_statusup_all([250, 250], [2000, 2000], -1)]),
+		proc: [ss_reinforcement_all(5, [ss_regenerate(0.2, 5, "RF"), ss_statusup_all([250, 250], [2000, 2000], -1)]),]
 	},
 	awakes: [
 		Panel_boost([0,1,0,0,0,],2),
@@ -69691,7 +69691,7 @@ Cards = [{
 	ss2: {
 		desc: "<精霊強化>5ターンの間、味方全体を徐々に回復しダメージを軽減(20%,20%)、さらに攻撃力とHPを500アップ(上限値:2000)◆発動中行動不可",
 		turn: 8,
-		proc: ss_reinforcement_all(5, [ss_regenerate(0.2, 5, "RF"), ss_attr_guard([1,1,1,1,1], 0.2, 5, "RF"), ss_statusup_all([500, 500], [2000, 2000], -1)]),
+		proc: [ss_reinforcement_all(5, [ss_regenerate(0.2, 5, "RF"), ss_attr_guard([1,1,1,1,1], 0.2, 5, "RF"), ss_statusup_all([500, 500], [2000, 2000], -1)]),]
 	},
 	Lawake: [
 		Statusup(500,0),
