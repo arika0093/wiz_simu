f2878ba3fde9951ef788dc85f24ab065c326f37c
Arika <Delete0093@Gmail.com>
2017-08-06 20:03:29 +0900

    L状態判定の処理を修正(ゾラス潜在の被ダメ上昇効果適用タイミングなどに影響)

--- js/simulator/initialize.js
+++ js/simulator/initialize.js
@@ -201,7 +201,6 @@ $(function () {
 				now.upval_hp = 0;
 				now.upval_atk = 0;
 				now.def_attr = card.attr;
-				now.lgstart_turn = -1;
 				now.target = [];
 				now.flags = {};
 				now.flags.enemy_counter = [];
@@ -213,7 +212,8 @@ $(function () {
 				now.ss_current = has_fastnum(ally);	// SSチャージターン
 				now.ss_isfirst = true;	// SSをまだ発動していないかどうか
 				now.ss_isboost = false;	// スキブを受けたかどうか
-				now.islegend = is_legendmode(card, now);
+				now.islegend = false; // Lモードかどうか(card.islegendとは意味合いが違うので注意)
+				now.lgstart_turn = -1;
 			}
 			// 空要素を詰める
 			als.Deck = $.grep(als.Deck, function (e) { return e !== undefined; });
