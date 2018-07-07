907bd09d302901c577caa1f72988020f477feb87
Arika <Delete0093@Gmail.com>
2018-07-07 23:32:11 +0900

    ・正月イーニアのL2を実装。 ・効果値持ち多段式カウンターに対応。

--- js/data/skill_enemy.js
+++ js/data/skill_enemy.js
@@ -707,7 +707,7 @@ function s_enemy_steal(dmg, tnum) {
 						teff.disabled = false;
 						teff.lim_turn = 0;
 						// L化
-						addQuizCorrectNum(fld, i, 999); // 盗むでMAXまで上昇するかは要検証
+						addQuizCorrectNum(fld, oi, 999); // 盗むでMAXまで上昇するかは要検証
 						nows[oi].ss_current = 999;
 						legend_timing_check(f, cards, nows, oi, false);
 					} else {
